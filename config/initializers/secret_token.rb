cf12c1696c064eb9b0a4565ba6d7985f42668c8c
Brantley Beaird <bbeaird@gmail.com>
2014-03-23 21:39:56 -0700

    Initial commit with rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuzzYouIn::Application.config.secret_key_base = 'afc638db2c5ff064901d8873e1297961d6a326f4d3637bdf6e6608f5b8ee4abc25aaf0d847e4499783b7a43efe3f82f4efab8adf0dae9d0e2f266e55debbe983'
