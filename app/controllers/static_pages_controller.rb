bc5f75d958e90099aec84a11700c189f6803daa1
Brantley Beaird <bbeaird@gmail.com>
2014-03-31 16:16:00 -0700

    Shows active visitor passes (from prior 4 hours) on home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,22 @@ class StaticPagesController < ApplicationController
     if user_signed_in?
       # @visitor_pass = current_user.visitor_passes.build
       @visitor_pass = VisitorPass.new
+      # @active_visitor_passes = current_user.visitor_passes
+      @active_visitor_passes = VisitorPass.where("user_id = ? AND created_at >= ?", current_user.id, (Time.now - 4.hours))
     end
+
+    session["counter"] ||= 0
+    sms_count = session["counter"]
+    if sms_count == 0
+      message = "Hello, thanks for the new message."
+    else
+      message = "Hello, thanks for message number #{sms_count + 1}"
+    end
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Sms message
+    end
+    session["counter"] += 1
+    p twiml.text
   end
 
   # private
