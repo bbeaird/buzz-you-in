9f0ab66dba6523d574cbe8ac4dfb933b867ebc9d
Brantley Beaird <bbeaird@gmail.com>
2014-04-08 16:46:19 -0700

    Changed phone_number to resident_phone_number in a few addtional places to make everything work.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def add_phone_numbers
     @user = current_user
-    @user.update(phone_number: params[:user][:phone_number], callbox_phone_number: params[:user][:callbox_phone_number])
+    @user.update(resident_phone_number: params[:user][:resident_phone_number], callbox_phone_number: params[:user][:callbox_phone_number])
     redirect_to root_url
   end
 end
