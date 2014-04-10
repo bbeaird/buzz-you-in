8b4bad2d0ddc7033e3ea28c6049a2cfdec0237ea
Brantley Beaird <bbeaird@gmail.com>
2014-04-10 16:05:03 -0700

    Removed resident_phone_number from visitor_passes model because it can be accessed via the relationship between visitor_passes and users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408233820) do
+ActiveRecord::Schema.define(version: 20140410225827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20140408233820) do
     t.string   "visitor_phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "resident_phone_number"
   end
 
 end
