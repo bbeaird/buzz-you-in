5be6e1bd8e51d0c19535bd805951ef043138d7b7
Brantley Beaird <bbeaird@gmail.com>
2014-06-12 18:25:49 -0700

    Moved returned phone number list to its own view file in attempt to get the returned list to show up. Still not quite working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :search_for_twilio_numbers
+
   def add_phone_numbers
     @user = current_user
     @user.update(resident_phone_number: params[:user][:resident_phone_number], callbox_phone_number: params[:user][:callbox_phone_number])
     redirect_to root_url
   end
 
-  def add_resident_byi_phone_number
-    @user = current_user
-    @user.update(resident_byi_phone_number: params[:user][:resident_byi_phone_number])
-    redirect_to root_url
-  end
+  # def add_resident_byi_phone_number
+  #   @user = current_user
+  #   @user.update(resident_byi_phone_number: params[:user][:resident_byi_phone_number])
+  #   redirect_to root_url
+  # end
 
   def search_for_twilio_numbers
     client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
@@ -20,22 +22,7 @@ class UsersController < ApplicationController
 
     begin
       local_numbers = client.account.available_phone_numbers.get('US').local
-      numbers =local_numbers.list(search_params)
-
-      unless numbers.empty?
-        out = '<html><head><title>Choose a number</title></head><body><h3>Choose a number</h3>'
-        numbers.each do |number|
-          out << "<form method='POST' action='/buy-number'>"
-          out << "<label>#{number.friendly_name}</label>"
-          out << "<input type='hidden' name='PhoneNumber' value='#{number.phone_number}' />"
-          out << "<input type='submit' value='BUY' /></form>"
-        end
-        out << '</body></html>'
-      else
-        '<b>Sorry!</b> Twilio doesn\'t have any numbers available that match those constraints.'
-      end
-    rescue StandardError => e
-      '<b>Sorry!</b> ' + e.message + '.'
+      @numbers =local_numbers.list(search_params)
     end
   end
 
