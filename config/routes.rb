d2ba056f77fd3ad24de40ea62990bcf621d6e250
Brantley Beaird <bbeaird@gmail.com>
2014-07-13 15:56:25 -0700

    Cleaned up routes a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 BuzzYouIn::Application.routes.draw do
 
-  get "registration/create"
   get 'static_pages/home'
+
   devise_for :users, controllers: { registrations: "my_registrations" }
   resources :visitor_passes, only: [:create, :index, :new, :destroy]
   resources :charges
-  # devise_scope :user do
-  #   root to: "devise/sessions#new"
-  # end
 
   root 'visitor_passes#index'
 
