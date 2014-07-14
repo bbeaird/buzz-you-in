f3c629424794c31de09d7199d81a4cd2fad5da5b
Brantley Beaird <bbeaird@gmail.com>
2014-07-13 17:16:19 -0700

    Setup people tracking in Mixpanel after signup.

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -1,6 +1,13 @@
 class MyRegistrationsController < Devise::RegistrationsController
   def create
     super
+    distinct_id = current_user.id
+    tracker = Mixpanel::Tracker.new(ENV['MIXPANEL_TOKEN'])
+    tracker.people.set(distinct_id, {
+      '$email' => current_user.email
+      })
+
+    p params
     p 'I can do it!!!!!!!!!!!!!!!'
   end
 end
