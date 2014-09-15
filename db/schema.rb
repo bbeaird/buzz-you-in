0554fc0d38061f6d9ca7462bb154e5fa46b285d0
Brantley Beaird <bbeaird@gmail.com>
2014-09-14 21:10:19 -0700

    Fixed password confirmation issue and added password digest.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915005247) do
+ActiveRecord::Schema.define(version: 20140915040420) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140915005247) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
+    t.string   "password_digest"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
