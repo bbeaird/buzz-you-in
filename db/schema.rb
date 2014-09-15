ede4316cd4edcb368ccc519676574a149a11f6e5
Brantley Beaird <bbeaird@gmail.com>
2014-09-14 17:54:39 -0700

    Added remember_token to Users table. Also added an index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807011632) do
+ActiveRecord::Schema.define(version: 20140915005247) do
 
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
   create_table "visitor_passes", force: true do |t|
     t.string   "visitor_phone_number"
